ef537a17aee6020119857c790eaad7c0a9b90eac
greenrok <kdr0112@gmail.com>
2022-08-16 16:42:03 +0900

    lecture

---
+++ LectureWEB/bin/src/main/webapp/js/join.js
@@ -0,0 +1,14 @@
+function registEvent() {
+	const handler = function(event) {
+		if (event.target.checked) {
+			alert('now check');
+		} else {
+			alert('now uncheck');
+		}
+	}
+	const boxes = document.getElementsByName("tools");
+	for (let i=0; i<boxes.length; i++) {
+		boxes[i].addEventListener('click', handler);
+	}
+}
+
